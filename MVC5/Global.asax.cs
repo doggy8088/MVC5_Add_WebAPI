4ad181e1744101b764a1ad0b25fbd0d00d67d4a9
Will Huang <doggy.huang@gmail.com>
2015-02-19 00:06:43 +0800

    修改 Global.asax.cs 檔案，註冊 ASP.NET Web API 相關設定

--- MVC5/Global.asax.cs
+++ MVC5/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
 
 namespace MVC5
 {
@@ -13,6 +14,8 @@ namespace MVC5
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            GlobalConfiguration.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
